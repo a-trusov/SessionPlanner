f55dc5739fa159e0584d9768154a9e9ce01c4d1e
Andrey Trusov <trusov@haulmont.com>
2020-02-02 16:39:08 +0400

    add Open Editor from Calendar

--- modules/web/src/com/company/sessionplanner/web/screens/session/SessionBrowse.java
+++ modules/web/src/com/company/sessionplanner/web/screens/session/SessionBrowse.java
@@ -1,11 +1,30 @@
 package com.company.sessionplanner.web.screens.session;
 
+import com.haulmont.cuba.gui.ScreenBuilders;
+import com.haulmont.cuba.gui.components.Calendar;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.sessionplanner.entity.Session;
+
+import javax.inject.Inject;
 
 @UiController("sessionplanner_Session.browse")
 @UiDescriptor("session-browse.xml")
 @LookupComponent("sessionsTable")
 @LoadDataBeforeShow
 public class SessionBrowse extends StandardLookup<Session> {
+    @Inject
+    private ScreenBuilders screenBuilders;
+
+    @Subscribe("sessionsCalendar")
+    public void onSessionsCalendarCalendarEventClick(Calendar.CalendarEventClickEvent event) {
+        Screen screen = screenBuilders.editor(Session.class, this)
+                .editEntity((Session)event.getEntity())
+                .withLaunchMode(OpenMode.DIALOG)
+                .build();
+
+        screen.addAfterCloseListener(afterCloseEvent -> {
+            getScreenData().loadAll();
+        });
+        screen.show();
+    }
 }
